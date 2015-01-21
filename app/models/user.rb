4502cff1b9879008d82f578d000f358155d8693f
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-01-21 13:39:10 -0800

    fixed length in validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,5 +19,5 @@ class User < ActiveRecord::Base
 
 	validates :name, presence: true
 	validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
-	validates :password, presence: true, length { in: 6..20 }, confirmation: true
+	validates :password, presence: true, length: { in: 6..20 }, confirmation: true
 end
