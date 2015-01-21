bfaf04476775ca59bf65fcbd9de9b2c8b4b34002
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-01-21 13:37:50 -0800

    added validations and password fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,8 @@ class User < ActiveRecord::Base
 			return false
 		end
 	end
+
+	validates :name, presence: true
+	validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+	validates :password, presence: true, length { in: 6..20 }, confirmation: true
 end
