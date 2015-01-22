aa4dcc2c36ee95ec48243d0111eb9a0cf84fd729
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-01-21 20:15:30 -0800

    devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,6 @@
 class User < ActiveRecord::Base
-	before_save { self.email = email.downcase }
-	validates :name, presence: true
-	validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
-	validates :password, presence: true, length: { in: 6..20 }, confirmation: true
-	attr_reader :password
-
-	def password=(unencrypted_password)
-		unless unencrypted_password.empty?
-		@password = unencrypted_password
-		self.password_digest = BCrypt::Password.create(unencrypted_password)
-		end
-	end
-
-	def authenticate(unencrypted_password)
-		if BCrypt::Password.new(self.password_digest) == unencrypted_password
-			return self
-		else
-			return false
-		end
-	end
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
