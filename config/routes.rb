d66d40df05ef1d30eccaa7ff75d52376e8a95c3d
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-01-22 17:24:35 -0800

    installed rspec and added sign up link

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   
   devise_for :users
   root "static_pages#home"
-  get '/signup' => 'users#new', as: :signup
-  get 'static_pages/home'
-
   get 'static_pages/about'
 
   
