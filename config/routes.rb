13b241cfd34ca59120e87ce73a58a029fb966079
An.nA <anna.git3@gmail.com>
2015-01-25 17:39:02 -0800

    update view and routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get 'static_pages/about'
   resources :users, only: [:show, :edit, :update, :index]
 
+  get '/linkedin/auth' => 'linkedin#auth'
+  get '/linkedin/callback' => 'linkedin#callback'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
