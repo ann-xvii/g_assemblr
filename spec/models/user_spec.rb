93d126ed3b1010753264de2f3e110953cd3bc604
Jonathan Thomas Fung <Jonathanth0mas@yahoo.com>
2015-01-26 14:59:54 -0800

    updated carousel

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe User, :type => :model do
   pending "add some examples to (or delete) #{__FILE__}"
 
   it "is invalid without a password" do
-  expect().to
+  expect().to be_invalid
 
   it "is invalid if the password is different from the password confirmation" do 
-  expect().to
+  expect().to 
   
   it "is invalid without an email" do
   expect().to
