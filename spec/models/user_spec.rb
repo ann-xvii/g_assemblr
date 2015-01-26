55d26dac5b7fd7bd5669701c41505e87a3b8137e
Jonathan Thomas Fung <Jonathanth0mas@yahoo.com>
2015-01-25 18:23:38 -0800

    specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is invalid without a password" do
+  expect().to
+
+  it "is invalid if the password is different from the password confirmation" do 
+  expect().to
+  
+  it "is invalid without an email" do
+  expect().to
+
+  it "cannot have the same credentials as another user" do
+  expect().to	
 end
