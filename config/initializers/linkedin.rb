0539a1a38fd65afeb9f06538a547a622794aaae2
An.nA <anna.git3@gmail.com>
2015-01-27 01:53:17 -0800

    fix uninitialized constant error for linkedin

--- config/initializers/linkedin.rb
+++ config/initializers/linkedin.rb
@@ -1,4 +1,5 @@
 
+require 'linkedin'
 # $client = LinkedIn::REST::Client.new do |config|
 #   config.consumer_key        = ENV['LINKEDIN_KEY']
 #   config.consumer_secret     = ENV['LINKEDIN_SECRET']
@@ -6,7 +7,15 @@
 #   config.access_token_secret = ENV['OAUTH_SECRET']
 # end
 
-$client = LinkedIn.configure do |config|
-  config.token = "ENV['LINKEDIN_KEY']"
-  config.secret = "ENV['LINKEDIN_SECRET']"
-end
+
+
+$client = LinkedIn::Client.new("ENV['LINKEDIN_KEY']", "ENV['LINKEDIN_SECRET']")
+
+$request_token = $client.request_token({}, :scope => "r_basicprofile+r_emailaddress")
+
+rtoken = request_token.token
+rsecret = request_token.secret
+
+request_token.authorize_url
+
+client.authorize_from_request(rtoken, rsecret, pin)
