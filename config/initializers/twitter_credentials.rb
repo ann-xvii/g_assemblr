adca0ace66ceef91e1b5d8a96bb4d1c52dc3e8b3
An.nA <anna.git3@gmail.com>
2015-01-26 17:41:34 -0800

    modify twitter credentials initializer

--- config/initializers/twitter_credentials.rb
+++ config/initializers/twitter_credentials.rb
@@ -1,11 +1,19 @@
 require 'twitter'
 
-Twitter.configure do |config|
-  config.consumer_key =  ENV['TWITTER_KEY']
-  config.consumer_secret = ENV['TWITTER_SECRET']
-  config.oauth_token = ENV['TWITTER_OAUTH_KEY']
-  config.oauth_token_secret = ENV['TWITTER_OAUTH_SECRET']
+# Twitter.configure do |config|
+#   config.consumer_key =  ENV['TWITTER_KEY']
+#   config.consumer_secret = ENV['TWITTER_SECRET']
+#   config.oauth_token = ENV['TWITTER_OAUTH_KEY']
+#   config.oauth_token_secret = ENV['TWITTER_OAUTH_SECRET']
 
 
 
+# end
+
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['TWITTER_KEY']
+  config.consumer_secret     = ENV['TWITTER_SECRET']
+  config.access_token        = ENV['TWITTER_OAUTH_KEY']
+  config.access_token_secret = ENV['TWITTER_OAUTH_SECRET']
 end
