89240110edc41afcfa6604925dd960b01a3da3d5
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-01-28 14:28:46 -0800

    GA profiles are showing

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,6 +6,8 @@ class ProfilesController < ApplicationController
 
 	def index
 
+		@appends = []
+
 		(1).upto(5).each do |i|
 
 			visit "https://profiles.generalassemb.ly/profiles?utf8=%E2%9C%93&roles%5B%5D=Front-end+Web+Developer&roles%5B%5D=Back-end+Web+Developer&roles%5B%5D=Full-stack+Web+Developer&roles%5B%5D=UX+Designer&roles%5B%5D=Information+Architect&roles%5B%5D=UX+Researcher&roles%5B%5D=Product+Manager&roles%5B%5D=Sales&positions%5B%5D=fulltime&positions%5B%5D=freelance&positions%5B%5D=apprenticeship&course=&end_date%5Bmin%5D=&end_date%5Bmax%5D=&work_location=&page=#{i}&nocache=25"
@@ -16,7 +18,8 @@ class ProfilesController < ApplicationController
 					title: profile.find("h3").text,
 					image: profile.find("img.index-profile-image")["src"]
 				}	
-			end 
+			end
+			@appends << @profiles 
 		end
 	end
 end
