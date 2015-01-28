31ecc22cd53806ecee6181f2f8786511f13de9ba
Jonathan Thomas Fung <Jonathanth0mas@yahoo.com>
2015-01-28 15:49:36 -0800

    no idea what changed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
 
   it "is invalid without a password" do
   expect().to be_invalid
