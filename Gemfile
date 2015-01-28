86e63a82a37c9ea28805bbebd8e5bbcbe866aa46
An.nA <anna.git3@gmail.com>
2015-01-28 14:37:13 -0800

    gemfile and linkedin update

--- Gemfile
+++ Gemfile
@@ -15,10 +15,13 @@ gem 'jbuilder', '~> 2.0'
 # SOCIAL GEMS
 gem 'therubyracer', platforms: :ruby
 gem 'devise'
+
 gem 'omniauth'
-gem 'linkedin', '~> 1.0.0'
+# gem 'linkedin', '~> 1.0.0'
 gem 'omniauth-linkedin'
 gem 'twitter', '~> 5.13.0'
+
+
 gem 'dotenv-rails', :groups => [:development, :test]
 gem 'feedjira', '~> 1.6.0'
 
--- config/initializers/linkedin.rb
+++ config/initializers/linkedin.rb
@@ -1,20 +1,4 @@
-
-Rails.application.config.middleware.use OmniAuth::Builder do    
-  provider :linkedin,
-           ENV['LINKEDIN_CONSUMER_KEY'],
-           ENV['LINKEDIN_CONSUMER_SECRET'],
-           :scope => 'r_fullprofile r_emailaddress r_contactinfo',
-           :fields => ["id", "email-address", "first-name", "last-name", "headline", "industry", "picture-url", "public-profile-url", "location", "positions"]
-end 
-
-
- require 'linkedin'
-# $client = LinkedIn::REST::Client.new do |config|
-# LINKEDIN_CONFIGURATION = { :site => 'https://api.linkedin.com',
-# :authorize_path => '/uas/oauth/authenticate',
-# :request_token_path =>'/uas/oauth/requestToken?scope=r_basicprofile+r_emailaddress',
-# :access_token_path => '/uas/oauth/accessToken' }
-
+ # require 'linkedin'
 
 # LinkedIn::Client.new do |config|
 #  config.consumer_key        = ENV['LINKEDIN_KEY']
@@ -24,10 +8,8 @@ end
 
 # end
 
-
-
 #$linkedin_client = LinkedIn::Client.new(ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET'])
-# $client = LinkedIn::Client.new(ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET'], LINKEDIN_CONFIGURATION)
+#$client = LinkedIn::Client.new(ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET'])
 
 #$request_token = $linkedin_client.request_token({}, :scope => "r_basicprofile r_emailaddress r_network")
 
