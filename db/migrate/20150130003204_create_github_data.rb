26c36988b6c1461c8f4ce972b652f080a00898f2
An.nA <anna.git3@gmail.com>
2015-01-29 16:36:45 -0800

    github data migration, edit schema

--- db/migrate/20150130003204_create_github_data.rb
+++ db/migrate/20150130003204_create_github_data.rb
@@ -1,7 +1,8 @@
 class CreateGithubData < ActiveRecord::Migration
   def change
     create_table :github_data do |t|
-
+      t.string  :username
+      t.integer :user_id
       t.timestamps null: false
     end
   end
