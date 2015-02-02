b11f29ac929f0cb2a2d4069d276a460e0bd13a0d
An.nA <anna.git3@gmail.com>
2015-02-02 00:45:00 -0800

    modify user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -37,16 +37,16 @@ RSpec.describe User, :type => :model do
   end
 
 
-  it "is invalid without a password" do
-  expect().to be_invalid
+  # it "is invalid without a password" do
+  # expect().to be_invalid
 
-  it "is invalid if the password is different from the password confirmation" do 
-  expect().to 
+  # it "is invalid if the password is different from the password confirmation" do 
+  # expect().to 
   
-  it "is invalid without an email" do
-  expect().to
+  # it "is invalid without an email" do
+  # expect().to
 
-  it "cannot have the same credentials as another user" do
-  expect().to	
+  # it "cannot have the same credentials as another user" do
+  # expect().to	
 
 end
