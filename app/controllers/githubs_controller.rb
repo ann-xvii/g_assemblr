a4b0b18e0884f80552b5a370b272b03b7b325a26
An.nA <anna.git3@gmail.com>
2015-02-02 10:50:42 -0800

    comment out github c

--- app/controllers/githubs_controller.rb
+++ app/controllers/githubs_controller.rb
@@ -1,17 +1,17 @@
-require 'github'
+# require 'github'
 
 
-gitty = Github.new
+# gitty = Github.new
 
-the_gitty = gitty.repos.list user: 'ann-xvii'
+# the_gitty = gitty.repos.list user: 'ann-xvii'
 
-# the gitty is an array of objects
+# # the gitty is an array of objects
 
 
-# this can give teh name of the repos
-# html_url gives the url
-the_gitty.each do |x|
-	length = x.html_url.length
-	# puts length
-	puts x.html_url[28..length]
-end
+# # this can give teh name of the repos
+# # html_url gives the url
+# the_gitty.each do |x|
+# 	length = x.html_url.length
+# 	# puts length
+# 	puts x.html_url[28..length]
+# end
