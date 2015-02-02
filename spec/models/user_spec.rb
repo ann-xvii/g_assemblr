3667c08ccd65d0a9e5ef3018464a7a4b98526cb2
An.nA <anna.git3@gmail.com>
2015-02-01 22:40:51 -0800

    update gemfile, add user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,12 @@ RSpec.describe User, :type => :model do
   # it "raises an error without a name" do
   # 	expect{subject.save!}.to raise_error(ActiveRecord::RecordInvalid)
   # end
+  it "has a valid factory" do
+    expect(FactoryGirl.build(:user)).to be_valid
+  end
+  
+  it "is invalid if email isn't formatted properly"
+  it "is invalid if a user tries to sign up with an email address that already exists in the database, regardless of capitalization"
 
   it "is invalid without an email address" do
   	user = FactoryGirl.build_stubbed(:no_email)
