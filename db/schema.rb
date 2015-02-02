f5c140838e64d25d04ad45737ac2df03459ecc1c
An.nA <anna.git3@gmail.com>
2015-02-02 02:14:57 -0800

    modify schema

--- db/schema.rb
+++ db/schema.rb
@@ -47,15 +47,12 @@ ActiveRecord::Schema.define(version: 20150130071523) do
     t.datetime "updated_at", null: false
   end
 
-
   create_table "statuses", force: :cascade do |t|
-    t.string   "body"
-    t.string   "status_updater"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.integer  "user_id"
+    t.string   "name"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
-
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
