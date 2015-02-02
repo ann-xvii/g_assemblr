d0b3dad13b39d0772a98e0d25bbbffc9a642b301
An.nA <anna.git3@gmail.com>
2015-02-01 22:59:02 -0800

    modify user spec, add email uniqueness validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,9 +15,20 @@ RSpec.describe User, :type => :model do
   it "has a valid factory" do
     expect(FactoryGirl.build(:user)).to be_valid
   end
-  
-  it "is invalid if email isn't formatted properly"
-  it "is invalid if a user tries to sign up with an email address that already exists in the database, regardless of capitalization"
+
+  it "is invalid if email isn't formatted properly" do
+    user = FactoryGirl.build(:user, email: "ads;eff")
+    expect(user).to be_invalid
+  end
+
+  it "is invalid if a user tries to sign up with an email address that already exists in our database, regardless of capitalization" do
+    FactoryGirl.create(:user, email: "percival@example.com")
+    FactoryGirl.create(:user, email: "PERCIVAL@example.com")
+    user = FactoryGirl.build(:user, email: "percival@example.com")
+    user1 = FactoryGirl.build(:user, email: "PERCIVAL@example.com")
+    expect(user).to be_invalid
+    expect(user1).to be_invalid
+  end
 
   it "is invalid without an email address" do
   	user = FactoryGirl.build_stubbed(:no_email)
