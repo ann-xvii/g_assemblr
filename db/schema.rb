17fa1f8187e055700bd47b7c67d4d5e5133a8569
Jonathan Thomas <Jonathanth0mas@yahoo.com>
2015-02-01 19:34:14 -0800

    adding slides

--- db/schema.rb
+++ db/schema.rb
@@ -47,13 +47,6 @@ ActiveRecord::Schema.define(version: 20150130071523) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "statuses", force: :cascade do |t|
-    t.string   "name"
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
